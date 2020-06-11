e14285ee400376272272ed5aacb3caee371db192
sunny-zuo <me@sunnyzuo.com>
2020-06-10 22:59:17 -0600

    feat: added navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,20 @@
 import React from "react"
 import Logo from "./logo"
 import { Link } from "gatsby"
+import "./navbar.css"
 
 export default function Navbar() {
     return (
-        <div>
+        <div className="navBar">
             <Link to="/"><Logo /></Link>
+            <ul>
+                <MenuItem to="/">Home</MenuItem>
+                <MenuItem to="/ctf-writeups">CTF Writeups</MenuItem>
+            </ul>
         </div>
     )
 }
+
+const MenuItem = props => (
+    <Link to={props.to}><li>{props.children}</li></Link>
+)
