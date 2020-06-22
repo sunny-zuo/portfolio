1418ab1e8d00ec1c49b2c1360fa6f32821c4c344
sunny-zuo <me@sunnyzuo.com>
2020-06-21 18:10:54 -0600

    feat: improve readability of path formatting

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,12 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 
     const ctfTemplate = require.resolve('./src/templates/ctfTemplate.js');
 
+    const formatPath = (ctf, title) => {
+        return `/ctf-writeups/` +
+        ctf.replace(/\s/g, "-").toLowerCase().replace(/[^a-zA-Z0-9-]/g, "") + "/" + // space -> dash, lowercase, remove non alphanumeric plus dash
+        title.replace(/\s/g, "-").toLowerCase().replace(/[^a-zA-Z0-9-]/g, "")
+    } 
+
     const result = await graphql(`{
         allMarkdownRemark(
             sort: { order: DESC, fields: [frontmatter___date]}
@@ -26,7 +32,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
         createPage({
-            path: `/ctf-writeups/${node.frontmatter.ctf.replace(/\s/g, "-").toLowerCase().replace(/[^a-zA-Z0-9-]/g, "")}/${node.frontmatter.title.replace(/\s/g, "-").toLowerCase().replace(/[^a-zA-Z0-9-]/g, "")}`,
+            path: formatPath(node.frontmatter.ctf, node.frontmatter.title),
             component: ctfTemplate,
             context: {
                 ctf: node.frontmatter.ctf,
