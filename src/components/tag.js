3a77bfb7040924ba7e7e46e5575ef7b6fa270355
sunny-zuo <me@sunnyzuo.com>
2020-06-26 21:56:40 -0600

    feat: added colored tags

--- src/components/tag.js
+++ src/components/tag.js
@@ -1,10 +1,31 @@
 import React from "react"
-import "./tag.css"
 
 export default function Tag(props) {
+    const colorArray = [
+        ["web", "#a56def"],
+        ["beginner", "#D81159"],
+        ["sql", "#70C1B3"],
+        ["xss", "#7d23ee"],
+        ["xxe", "#256def"],
+        ["lfi", "#bb342f"],
+        ["json", "#ee7674"],
+        ["misc", "#00af54"],
+        ["osint", "#544e61"],
+        ["jwt", "#5cc8ff"]
+    ];
+    const colorMap = new Map(colorArray);
+
+    const tagStyle = {
+        backgroundColor: colorMap.get(props.tag),
+        color: "white",
+        padding: "4px 10px",
+        borderRadius: "24px",
+        fontSize: "12px",
+        fontFamily: "'Roboto', sans-serif"
+    }
     return (
         <div className="tag">
-            <span>{props.tag}</span>
+            <span style={tagStyle}>{props.tag}</span>
         </div>
     )
 }
