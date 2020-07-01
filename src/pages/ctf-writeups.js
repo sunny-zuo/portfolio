6ed5a38a5effa2ff803e78dc4531808f5fa4b296
sunny-zuo <me@sunnyzuo.com>
2020-06-30 20:50:30 -0600

    feat: use query string to filter writeups by tag or CTF

--- src/pages/ctf-writeups.js
+++ src/pages/ctf-writeups.js
@@ -4,9 +4,13 @@ import Layout from "../components/layout";
 import CTFCard from "../components/ctfcard";
 import PageHelmet from "../components/pageHelmet";
 import { graphql } from 'gatsby';
+import { useLocation } from '@reach/router';
+import queryString from 'query-string';
 
 export default function CTFWriteups({data}) {
-    const Posts = data.allMarkdownRemark.edges.map(edge => <CTFCard title={edge.node.frontmatter.title} date={edge.node.frontmatter.date} ctf={edge.node.frontmatter.ctf} tags={edge.node.frontmatter.tags} />)
+    const location = useLocation();
+    const searchQuery = getSearchQueries(location.search);
+    const Posts = data.allMarkdownRemark.edges.filter(edge => matchSearch(edge, searchQuery)).map(edge => <CTFCard title={edge.node.frontmatter.title} date={edge.node.frontmatter.date} ctf={edge.node.frontmatter.ctf} tags={edge.node.frontmatter.tags} key={edge.node.frontmatter.title}/>)
     return (
         <Layout>
             <PageHelmet title="CTF Writeups" />
@@ -16,6 +20,31 @@ export default function CTFWriteups({data}) {
                 </div>
         </Layout>
     )
+}
+
+const matchSearch = (edge, searchQuery) => {
+    if (searchQuery) {
+        const { tag, ctf } = searchQuery;
+        if (tag) {
+            if (!edge.node.frontmatter.tags.includes(tag.toLowerCase())) {
+                return false;
+            }
+        }
+        if (ctf) {
+            if (edge.node.frontmatter.ctf.toLowerCase() !== ctf.toLowerCase()) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return true;
+}
+
+const getSearchQueries = (query) => {
+    if (query) {
+        const searchQuery = queryString.parse(query);
+        return searchQuery;
+    }
 }
 
 export const pageQuery = graphql`{
